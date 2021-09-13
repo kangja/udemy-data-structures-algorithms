b28619292df6f8ab709c68310aa0993a9494ff4c
Jason <kangja1996@gmail.com>
2021-09-13 17:04:22 -0400

    progress

---
+++ linked-list.js
@@ -0,0 +1,14 @@
+let myLinkedList = {
+  head: {
+    value: 10,
+    next: {
+      value: 5,
+      next: {
+        value: 16,
+        next: null,
+      },
+    },
+  },
+};
+
+console.log(myLinkedList);
