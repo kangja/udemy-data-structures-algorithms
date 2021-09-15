a5fcfbca0ce2c0cb11ec03ae1f0618de3c3a0a0f
Jason <kangja1996@gmail.com>
2021-09-15 01:09:38 -0400

    progress

--- linked-list.js
+++ linked-list.js
@@ -15,23 +15,38 @@ class LinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      //   next: null,
+      next: null,
     };
-    // this.tail = this.head;
-    // this.length = 1;
+    this.tail = this.head;
+    this.length = 1;
   }
   append(value) {
     //   code here
-    this.next = {
-      next: value,
+    const newNode = {
+      value: value,
+      next: null,
     };
-    this.next = value;
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    // code here
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
   }
 }
 
 const myLinkedList = new LinkedList(10);
-
 myLinkedList.append(5);
 myLinkedList.append(16);
-
+myLinkedList.prepend(1);
 console.log(myLinkedList);
